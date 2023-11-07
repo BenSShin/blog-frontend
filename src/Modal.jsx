3556d5bbf9a286f490d6b3f26fe5d73ce2ea42a5
bshin27 <125755440+bshin27@users.noreply.github.com>
2023-11-07 14:06:26 -0600

    added eslint rule

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import "./Modal.css";
 
 export function Modal(props) {
--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 export function PostsIndex(props) {
   console.log(props);
 
