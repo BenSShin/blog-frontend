8e4462b709a8d3bf97ca93e0594b1f16415dab07
bshin27 <125755440+bshin27@users.noreply.github.com>
2023-11-09 12:32:58 -0600

    added bootstrap and styled header

--- src/Content.jsx
+++ src/Content.jsx
@@ -33,7 +33,7 @@ export function Content() {
   useEffect(handleIndexPosts, []);
 
   return (
-    <div>
+    <div className="container">
       <PostsNew />
       <PostsIndex posts={posts} onShowPost={handleShowPost} />
       <Modal show={isPostsShowVisible} onClose={handleClose}>
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,30 @@
 export function Header() {
   return (
     <header>
-      <a href="#">Home</a> | <a href="#posts-index">All posts</a> | <a href="#posts-new">New post</a>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Navbar
+          </a>
+          <a className="nav-link active" href="#posts-new">
+            New Post
+          </a>
+          <a className="nav-link active" href="#posts-index">
+            All Posts
+          </a>
+          <div className="col-2"></div>
+          <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
     </header>
   );
+}
+
+{
+  /* <a href="#">Home</a> | <a href="#posts-index">All posts</a> | <a href="#posts-new">New post</a> */
 }
