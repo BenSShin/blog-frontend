8284ab98cdc434607b63bfc0417de90ab6a2b504
bshin27 <125755440+bshin27@users.noreply.github.com>
2023-11-09 14:29:33 -0600

    changed new post so stays on page

--- src/PostShow.jsx
+++ src/PostShow.jsx
@@ -1,15 +1,10 @@
 export function PostsShow(props) {
   return (
-    <div className="row row-cols-1 row-cols-md-2 g-4">
-      <div className="col">
-        <div className="card">
-          <img src="props.post.image" className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">{props.post.title}</h5>
-            <p className="card-text">{props.post.image}</p>
-          </div>
-        </div>
-      </div>
+    <div id="posts-show">
+      <h1>Posts Info</h1>
+      <h2>{props.post.title}</h2>
+      <p>{props.post.body}</p>
+      <p>{props.post.image}</p>
     </div>
   );
 }
--- src/PostsNew.jsx
+++ src/PostsNew.jsx
@@ -1,9 +1,24 @@
+import axios from "axios";
+
 export function PostsNew() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    axios
+      .post("http://localhost:3000/posts.json", params)
+      .then((response) => {
+        console.log(response.data);
+        event.target.reset();
+      })
+      .catch((error) => {
+        console.log(error.response.data.errors);
+      });
+  };
   return (
     <div id="posts-new">
       <h1>New post</h1>
       {/* new blog post */}
-      <form method="POST" action="http://localhost:3000/posts.json">
+      <form onSubmit={handleSubmit}>
         <div className="input-group mb-3">
           <span className="input-group-text" id="inputGroup-sizing-default">
             Title:
